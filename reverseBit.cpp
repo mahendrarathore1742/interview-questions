af5cf25177c870d1b1f8f821967753980ce32fb8
mr-robot <mahendrarathore1742@gmail.com>
2021-05-09 14:34:52 +0530

    add code

---
+++ reverseBit.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace  std;
+
+ unsigned int reversBit( unsigned int A){
+
+	int ans=0;
+
+	for (int i = 31; i >=0; i--)
+	{
+		ans |=(A &1)<<i;
+		A>>=1;	
+	}
+
+	return ans;
+}
+
+int main(){
+
+	cout<<reversBit(3)<<endl;
+	return 0;
+}
