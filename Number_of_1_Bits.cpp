932b2dd73ce48d9847ade0966c0a1d45fb9d808f
mr-robot <mahendrarathore1742@gmail.com>
2021-05-09 14:17:35 +0530

    add code

---
+++ Number_of_1_Bits.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace  std;
+
+int Number_of_1_Bits(int A){
+
+	int count=0;
+
+	while(A>0){
+		A=A&(A-1);
+		count++;
+
+	}
+
+	return count;
+
+}
+
+int main(){
+
+	cout<<Number_of_1_Bits(00000000000000000000000000001011)<<endl;
+	return 0;
+}
