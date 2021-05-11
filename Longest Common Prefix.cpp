112294406cd3497d8622624a9ac9564195d863bd
mr-robot <mahendrarathore1742@gmail.com>
2021-05-11 14:31:07 +0530

    add code

---
+++ Longest Common Prefix.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace  std;
+
+string longestCommonPrefix(vector<string> &A){
+
+	 if(A.size()<=1)
+        return A[0];
+    
+    string res = "";
+    sort(A.begin(), A.end());
+    
+    int n = A.size()-1, i=0, m = A[0].size();
+    
+    // Keep adding characters till they are same.
+    while(i<m && A[0][i] == A[n][i]){
+        res += A[0][i];
+        i++;
+    }
+    return res;
+	}
+
+int main(){
+
+	std::vector<string> v={"abcdefgh", "aefghijk", "abcefgh"};
+	cout<<longestCommonPrefix(v)<<endl;
+
+}
