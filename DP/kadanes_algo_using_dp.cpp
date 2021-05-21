ddc88c938526e951a1fb1758139c8d2c5d7a9705
mr-robot <mahendrarathore1742@gmail.com>
2021-05-21 14:37:39 +0530

    add code

---
+++ DP/kadanes_algo_using_dp.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace  std;
+#define vi vector<int>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define  rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+
+
+const int N=1e5+2;
+
+
+int main(){
+	int n;
+	cin>>n;
+
+	vi a(n);
+
+	rep(i,0,n)
+		cin>>a[i];
+
+
+	int curr=0,Max=0;
+
+
+	rep(i,0,n){
+		curr+=a[i];
+
+		Max=max(Max,curr);
+		if(curr<0)
+			curr=0;
+	}
+
+	cout<<Max<<endl;
+
+	return 0;
+}
