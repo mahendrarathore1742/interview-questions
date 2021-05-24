80ecdd86b167df1c52f146c807d86679f6777b38
mr-robot <mahendrarathore1742@gmail.com>
2021-05-24 12:40:57 +0530

    add code

--- DP/optimal_game_strategy_in_dp.cpp
+++ DP/optimal_game_strategy_in_dp.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 vector<int> a;
 
+int dp[100][100];
+
 int solve(int i,int j){
 	if(i==j){
 		return a[i];
@@ -12,29 +14,28 @@ int solve(int i,int j){
 		return 0;
 	}
 
+	if(dp[i][j]!=-1){
+		return dp[i][j];
+	}
+
 
 	int l=a[i] + min(solve(i+2,j),solve(i+1,j-1));
 	int r=a[j]+min(solve(i,j-2),solve(i+1,j-1));
 
-	return max(l,r);
+	dp[i][j]= max(l,r);
+
+	return dp[i][j];
 }
 
 
 int main(){
-
+	memset(dp,-1,sizeof(dp));
 	int n;
 	cin>>n;
-
 	a=vector<int>(n);
-
-
 	for(auto &i :a){
 		cin>>i;
 	}
-
-
 	cout<<solve(0,n-1)<<endl;
-
-
 	return 0;
 }
