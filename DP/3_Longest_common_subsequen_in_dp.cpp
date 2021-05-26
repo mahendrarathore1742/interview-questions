294b06308cf33c27549df7a77a63924024c0585c
mr-robot <mahendrarathore1742@gmail.com>
2021-05-26 11:57:26 +0530

    add code

--- DP/3_Longest_common_subsequen_in_dp.cpp
+++ DP/3_Longest_common_subsequen_in_dp.cpp
@@ -7,7 +7,8 @@ using namespace  std;
 #define ff first
 #define ss second
 
-
+const int  N=205;
+int dp[N][N][N];
 
 int lcs(string &s1,string &s2,string &s3,int i,int j,int k){
 
@@ -15,26 +16,47 @@ int lcs(string &s1,string &s2,string &s3,int i,int j,int k){
 		return 0;
 	}
 
+	if(dp[i][j][k]!=-1){
+		return dp[i][j][k];
+	}
 
 	if(s1[i-1]==s2[j-1] and s2[j-1] == s3[k-1]){
-
-		return 1+lcs(s1,s2,s3,i-1,j-1,k-1);
+		dp[i][j][k]= 1+lcs(s1,s2,s3,i-1,j-1,k-1);
 	}
+	else{
+
 	int l=lcs(s1,s2,s3,i-1,j,k);
 	int r=lcs(s1,s2,s3,i,j-1,k);
 	int p=lcs(s1,s2,s3,i-1,j,k-1);
-	return max({l,r,p});
-}
 
+	dp[i][j][k]= max({l,r,p});
+
+	}
+
+	return dp[i][j][k];
+}
 
 
 int main(){
 
-	string s1="mahed",s2="dema",s3="apma";
+	rep(i,0,N){
+		rep(j,0,N){
+			
+			rep(k,0,N){
+				dp[i][j][k]=-1;
+			}
+		}
+	}
+
+
+	string s1="mahedm",s2="demam",s3="apmam";
 	
 	int i=s1.size();
 	int j=s2.size();
 	int k=s3.size();
 	cout<<lcs(s1,s2,s3,i,j,k)<<endl;
+	return 0;
+
+
 	return 0;
 }
