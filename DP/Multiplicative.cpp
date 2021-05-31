48abf2174eb946094f6107b13fb63583db49ad05
mr-robot <mahendrarathore1742@gmail.com>
2021-05-31 20:27:01 +0530

    add code

--- DP/Multiplicative.cpp
+++ DP/Multiplicative.cpp
@@ -4,17 +4,12 @@ using namespace  std;
 #define int long long 
 
 const int N=1e5+2 ,MOD=1e9+7;
-
-
-
 class triplet{
 public:
 	int x,y,gcd;
-
 };
 
 triplet extendedEuclid(int a,int b){
-
 	if(b==0){
 		triplet ans;
 		ans.x=1;
@@ -22,19 +17,27 @@ triplet extendedEuclid(int a,int b){
 		ans.y=0;
 		return ans;
 	}
-
 	triplet smallans=extendedEuclid(b,a%b);
 	triplet ans;
 	ans.gcd=smallans.gcd;
 	ans.x= smallans.y;
 	ans.y=smallans.x-(a/b)*smallans.y;
 	return ans;
-} 
+}
+
+int multiplicativeModuloInverse(int a,int m){
+
+	triplet temp=extendedEuclid(a,m);
+
+	return temp.x;
+
+}
 
 signed main(){
 
-	int a=5,b=3;
-	triplet ans=extendedEuclid(a,b);
-	cout<<ans.gcd<<" "<<ans.x<<" "<<ans.y<<endl;
+	int a=17,m=3;
+
+	cout<<multiplicativeModuloInverse(a,m)<<endl;
+
 	return 0;
 }
