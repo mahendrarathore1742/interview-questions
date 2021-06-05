fcf67794f47e485cccbb7ccf25a5d0c740591dfd
mr-robot <mahendrarathore1742@gmail.com>
2021-06-05 13:50:11 +0530

    add code

--- DP/Binary_Exponentiation2.cpp
+++ DP/Binary_Exponentiation2.cpp
@@ -8,21 +8,20 @@ int power(int a,int n){
 
 	if(n==0)
 		return 1;
-
 	int p=(power(a,n/2)%MOD);
 
 	if(n&1)
 		return (((p*p)%MOD)*a)%MOD;
 	else
 		return (p*p)%MOD;
+
+
 }
 
 signed main(){
-
 	int a=2,n=6;
-
 	a%MOD;
-
 	cout<<power(a,n)<<endl;
 
+	return 0;
 }
