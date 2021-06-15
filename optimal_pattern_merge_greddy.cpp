e100360a7cbb2f5c46065a6d6758ec14e74f64ab
mr-robot <mahendrarathore1742@gmail.com>
2021-06-15 14:22:51 +0530

    add code

--- optimal_pattern_merge_greddy.cpp
+++ optimal_pattern_merge_greddy.cpp
@@ -11,17 +11,13 @@ int main(){
 	{
 		cin>>a[i];
 	}
-
 	priority_queue<int,vector<int>, greater<int> > minheap;
-	
 	for (int i = 0; i <n; ++i)
 	{
 		minheap.push(a[i]);
 	}
 
-
 	int ans=0;
-
 	while(minheap.size()>1)
 	{
 		int e1=minheap.top();
@@ -30,11 +26,7 @@ int main(){
 		minheap.pop();
 		ans+=e1+e2;
 		minheap.push(e1+e2);
-
 	}
 	cout<<ans<<endl;
-
-
-
 	return 0;
 }
